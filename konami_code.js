a77cb6600ca4b9e62976ea648f69222fc29b8863
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-27 00:34:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0;
+
 
 function init() {
+  let index = 0;
   // This is the function that would be invoked by the event listener.
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
