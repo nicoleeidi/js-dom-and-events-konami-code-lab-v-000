5076ee9de463795fda1e67396597668d067548d4
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-27 00:35:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0;
   // This is the function that would be invoked by the event listener.
-  function onKeyDownHandler(e) {
+  document.body.addEventListener('keydown',function(e){})
     const key = parseInt(e.detail || e.which);
 
     if (key === code[index]) {
@@ -18,7 +18,7 @@ function init() {
     } else {
       index = 0;
     }
-  }
+
 
   // Write your JavaScript code inside the init() function
 
